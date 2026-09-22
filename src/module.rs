a17d004d4bd4512d7e52334d62acfa074d497b8a
unknown <mehradhetfield@gmail.com>
2026-09-22 03:57:18 +0330

    chore: modul structure

--- src/module.rs
+++ src/module.rs
@@ -8,14 +8,21 @@ pub struct Dependency {
 pub struct Module {
     pub id: String,
     pub source: String,
+    pub transformed_source: String,
     pub dependencies: Vec<Dependency>,
 }
 
 impl Module {
-    pub fn new(id: String, source: String, dependencies: Vec<Dependency>) -> Self {
+    pub fn new(
+        id: String,
+        source: String,
+        transformed_source: String,
+        dependencies: Vec<Dependency>,
+    ) -> Self {
         Self {
             id,
             source,
+            transformed_source,
             dependencies,
         }
     }
