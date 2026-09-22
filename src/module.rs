12dac7ab51061bd1f865264739d96213ef033563
unknown <mehradhetfield@gmail.com>
2026-09-22 23:58:55 +0330

    chore: export type def

--- src/module.rs
+++ src/module.rs
@@ -5,12 +5,18 @@ pub struct Dependency {
 }
 
 #[derive(Debug)]
+pub enum Export {
+    Named(String),
+    Default(String),
+}
+
+#[derive(Debug)]
 pub struct Module {
     pub id: String,
     pub source: String,
     pub transformed_source: String,
     pub dependencies: Vec<Dependency>,
-    pub exports: Vec<String>,
+    pub exports: Vec<Export>,
 }
 
 impl Module {
@@ -19,7 +25,7 @@ impl Module {
         source: String,
         transformed_source: String,
         dependencies: Vec<Dependency>,
-        exports: Vec<String>,
+        exports: Vec<Export>,
     ) -> Self {
         Self {
             id,
